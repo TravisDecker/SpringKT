7d877c89b06ad8dc8a782000a885cff8ca778af9
TravisDecker <decker4693@gmail.com>
2024-07-23 08:18:57 -0600

    Use Repo Bean

--- src/main/kotlin/com/decker/springkt/randomresponse/RandomResponseController.kt
+++ src/main/kotlin/com/decker/springkt/randomresponse/RandomResponseController.kt
@@ -1,13 +1,15 @@
 package com.decker.springkt.randomresponse
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RequestMapping("/random-response")
 @RestController
 class RandomResponseController(private final val randomResponseRepo: RandomResponseRepo) {
 
     @GetMapping
     private fun getRandomResponse(): String {
-        return "Random response";
+        return randomResponseRepo.getRandomResponse();
     }
 }
